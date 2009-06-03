c2021e2dcfea7cb2100b09677551219f75d689c3
Josh Nichols <josh@technicalpickles.com>
2009-06-02 20:52:35 -0400

    Added a base from which to test.

--- lib/feedra.rb
+++ lib/feedra.rb
@@ -1,5 +1,6 @@
 require 'feedzirra'
 
+require 'summifire'
 require 'feedra/feed'
 require 'feedra/entry'
 require 'feedra/extensions/feedzirra'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,29 @@
 require 'spec'
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+here = File.dirname(__FILE__)
+$LOAD_PATH.unshift here
+$LOAD_PATH.unshift "#{here}/../lib"
+
+require 'activerecord'
+
 require 'feedra'
 
+ActiveRecord::Migration.verbose = false
+RAILS_ROOT = here
+RAILS_DEFAULT_LOGGER = Logger.new("#{RAILS_ROOT}/test.log") 
+
 Spec::Runner.configure do |config|
+
+  config.before(:all) do
+    FileUtils.mkdir_p "#{here}/migrate"
+    FileUtils.cp "#{here}/../generators/feedra/templates/migration.rb", "#{here}/migrate/001_create_feed_and_events.rb"
+
+    ActiveRecord::Base.logger = Logger.new("#{here}/activerecord_base.log")
+
+    ActiveRecord::Base.establish_connection :adapter => 'sqlite3',
+                                            :database => ':memory:'
+    
+    ActiveRecord::Migrator.migrate "#{here}/migrate"
+  end
   
 end
