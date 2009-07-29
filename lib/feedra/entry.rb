8e6d4414884034b42d40f7bf8fedf8700af24759
Josh Nichols <josh@technicalpickles.com>
2009-07-28 23:17:12 -0400

    Moved logic for finding an Entry matching a FeedZirra entry into Entry.

--- lib/feedra/entry.rb
+++ lib/feedra/entry.rb
@@ -3,6 +3,7 @@ module Feedra
     
     def self.included(other)
       other.class_eval do
+        extend ClassMethods
         validates_presence_of :checksum
         belongs_to :feed
       end
@@ -16,6 +17,13 @@ module Feedra
 
     def summarized(options = {})
       Summifire.new(self.summary, self.content).to_s(options)
+    end
+
+    module ClassMethods
+      def find_by_feedzirra_entry(feedzirra_entry)
+        find_by_title_and_url(feedzirra_entry.title, feedzirra_entry.url)
+      end
+
     end
 
   end
